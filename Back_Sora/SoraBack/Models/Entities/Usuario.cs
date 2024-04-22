01d441e130eac37f8331f9047ffe50329fac6c50
David Ortiz Corchero <david140303@hotmail.es>
2024-04-22 20:36:39 +0200

    Implementación de la primera vista y de iniciar sesion

--- Back_Sora/SoraBack/Models/Entities/Usuario.cs
+++ Back_Sora/SoraBack/Models/Entities/Usuario.cs
@@ -4,7 +4,7 @@
     {
         public int UsuarioId { get; set; }
         public string NombreCuenta { get; set; }
-        public string NombreCorreo { get; set; }
+        public string NombreUsuario { get; set; }
         public string Correo { get; set; }
         public string Contrasena { get; set; }
         public string descripcion { get; set; }
