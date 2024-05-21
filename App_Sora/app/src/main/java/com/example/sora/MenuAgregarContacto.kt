fda410f4c1fa62c35e480b7323b03e85b2f0365d
David Ortiz Corchero <david140303@hotmail.es>
2024-05-21 11:45:00 +0200

    .

---
+++ App_Sora/app/src/main/java/com/example/sora/MenuAgregarContacto.kt
@@ -0,0 +1,20 @@
+package com.example.sora
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MenuAgregarContacto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu_agregar_contacto)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
